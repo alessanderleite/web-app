4850de83138e91b9b0116a6af06ecf357e38b2c5
alessanderleite <alessander.logistica@gmail.com>
2019-11-17 10:54:31 -0300

    add constructors, getters and setters

--- src/main/java/br/com/alessanderleite/entity/User.java
+++ src/main/java/br/com/alessanderleite/entity/User.java
@@ -1,5 +1,65 @@
 package br.com.alessanderleite.entity;
 
-public class User {
+import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class User implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8728234736529431507L;
+	
+	@Id
+	@Column
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer userId;
+	
+	@Column
+	private String userName;
+	
+	@Column
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	private List<Skill> skills = new LinkedList<Skill>();
+
+	public User() {}
+
+	public User(String userName, List<Skill> skills) {
+		this.userName = userName;
+		this.skills = skills;
+	}
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public List<Skill> getSkills() {
+		return skills;
+	}
+
+	public void setSkills(List<Skill> skills) {
+		this.skills = skills;
+	}
 }
