4627f43143a9f42c6ab24c64a57df668516bf0bf
alessanderleite <alessander.logistica@gmail.com>
2019-11-17 11:02:49 -0300

    create the entity class named Skill, add constructors, getters and setters

---
+++ src/main/java/br/com/alessanderleite/entity/Skill.java
@@ -0,0 +1,59 @@
+package br.com.alessanderleite.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Skill {
+	
+	@Id
+	@Column
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer skillId;
+	
+	@Column
+	private String skillName;
+	
+	@Column
+	@ManyToOne
+	private User user;
+
+	public Skill() {}
+	
+	public Skill(String skillName) {
+		this.skillName = skillName;
+	}
+
+	public Skill(String skillName, User user) {
+		this.skillName = skillName;
+		this.user = user;
+	}
+
+	public Integer getSkillId() {
+		return skillId;
+	}
+
+	public void setSkillId(Integer skillId) {
+		this.skillId = skillId;
+	}
+
+	public String getSkillName() {
+		return skillName;
+	}
+
+	public void setSkillName(String skillName) {
+		this.skillName = skillName;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+}
